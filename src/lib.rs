921c09ad6e48cfb4652e9f352d44afae0a56183e
CableSynth <lorencalebbaum@gmail.com>
2022-06-28 21:41:13 -0400

    fmt, add more needed functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,18 @@
 use std::vec;
 
-use petgraph::{matrix_graph::{UnMatrix, NotZero}};
+use petgraph::matrix_graph::{NotZero, UnMatrix};
 #[derive(Debug, Clone, Copy)]
 pub struct Point([f32; 2]);
-
-impl Point {
-    fn substract (
-        
-    )
-}
 
 #[derive(Clone, Copy)]
 pub struct Circle {
     location: Point,
-    radius: f32
+    radius: f32,
 }
 
 impl Circle {
-    fn new (
-        location: Point,
-        radius: f32
-    ) -> Self {
-        Self { 
-            location, 
-            radius
-        }
+    fn new(location: Point, radius: f32) -> Self {
+        Self { location, radius }
     }
 }
 #[derive(Debug)]
@@ -32,16 +20,14 @@ pub struct Node {
     location: Point,
 }
 impl Node {
-    fn new(
-        location: Point
-    ) -> Self{
+    fn new(location: Point) -> Self {
         Self { location }
     }
 }
 
 pub struct Edge {
     edge_type: EdgeType,
-    weight: f32
+    weight: f32,
 }
 
 enum EdgeType {
@@ -53,7 +39,11 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
-pub fn build_graph(start: Node, end: Node, zones: Vec<Circle>) -> UnMatrix<Node, Edge, Option<Edge>>{
+pub fn build_graph(
+    start: Node,
+    end: Node,
+    zones: Vec<Circle>,
+) -> UnMatrix<Node, Edge, Option<Edge>> {
     let mut graph = UnMatrix::<Node, Edge, Option<Edge>>::with_capacity(32);
     graph.add_node(start);
     graph.add_node(end);
@@ -61,14 +51,37 @@ pub fn build_graph(start: Node, end: Node, zones: Vec<Circle>) -> UnMatrix<Node,
 }
 
 pub fn line_of_sight(node_1: Node, node_2: Node, zone: Circle) -> bool {
-
+    /// Calculate u
+    let c = zone.location.0;
+    let a = node_1.location.0;
+    let b = node_2.location.0;
     return false;
 }
 
-pub fn vector_norm(p1: Point, p2: Point) {
-    let dot: f32 = p1.0.iter().zip(p2.0.iter()).map(|(a, b)| a * b).sum();
+pub fn dot_product(p1: Vec<f32>, p2: Vec<f32>) -> f32 {
+    let dot: f32 = p1.iter().zip(p2.iter()).map(|(a, b)| a * b).sum();
+    return dot;
 }
 
+pub fn distance(p1: Vec<f32>, p2: Vec<f32>) -> f32 {
+    let square_sum: f32 = p1
+        .iter()
+        .zip(p2.iter())
+        .map(|(x1, x2)| (x2 - x1).powi(2))
+        .sum();
+    let distance = square_sum.sqrt();
+    return distance;
+}
+
+pub fn add_pts(p1: Vec<f32>, p2: Vec<f32>) -> Vec<f32> {
+    let point_sum: Vec<f32> = p1.iter().zip(p2.iter()).map(|(x1, x2)| x1 + x2).collect();
+    return point_sum;
+}
+
+pub fn subtrac_pts(p1: Vec<f32>, p2: Vec<f32>) -> Vec<f32> {
+    let difference: Vec<f32> = p1.iter().zip(p2.iter()).map(|(x1, x2)| x1 - x2).collect();
+    return difference;
+}
 
 #[cfg(test)]
 mod tests {
